f618368f826492128eef6cac86fbc76fb9784040
unknown <samara1380@gmail.com>
2026-04-11 17:50:12 -0300

    feat: criacao das entidades de dominio

---
+++ Projeto_Locadora/LocadoraVeiculosApi/Models/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraVeiculosApi.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(11, MinimumLength = 11)]
+        public string CPF { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(120)]
+        public string Email { get; set; } = string.Empty;
+
+        [StringLength(20)]
+        public string? Telefone { get; set; }
+
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
+
+        public ICollection<Aluguel> Alugueis { get; set; } = new List<Aluguel>();
+    }
+}
